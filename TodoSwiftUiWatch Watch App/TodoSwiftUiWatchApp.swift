ab483053d5a8eb1ffea61cd50acd51a6715681b0
Carlos Umaña Acevedo <carumtito@gmail.com>
2023-08-01 22:58:09 -0600

    Support tvOS and watchOS

---
+++ TodoSwiftUiWatch Watch App/TodoSwiftUiWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoSwiftUiWatchApp.swift
+//  TodoSwiftUiWatch Watch App
+//
+//  Created by Carlos Eduardo Umaña Acevedo on 1/8/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoSwiftUiWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
