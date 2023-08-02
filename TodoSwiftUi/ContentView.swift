278fc5d5f0ab34ea67fe3d893c8e92f6082a0d3c
Carlos Umaña Acevedo <carumtito@gmail.com>
2023-08-01 22:09:35 -0600

    Initial Commit

---
+++ TodoSwiftUi/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TodoSwiftUi
+//
+//  Created by Carlos Eduardo Umaña Acevedo on 1/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TodoSwiftUi/TodoSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoSwiftUiApp.swift
+//  TodoSwiftUi
+//
+//  Created by Carlos Eduardo Umaña Acevedo on 1/8/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
